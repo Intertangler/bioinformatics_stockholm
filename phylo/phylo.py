7627d76db35b1f569104829d622f759387430b9a
Intertangler <ian.hoffecker@gmail.com>
2021-03-07 19:24:18 +0100

    add myalign to phylo

--- phylo/phylo.py
+++ phylo/phylo.py
@@ -1,3 +1,66 @@
+class MyAlign:
+
+    def __init__(self, lseqs, al_type = "protein"):
+        self.listseqs = lseqs
+        self.al_type = al_type
+    
+    def __len__(self): # number of columns
+        return len(self.listseqs[0])
+    
+    def __getitem__(self, n):
+        if type(n) is tuple and len(n) ==2: 
+            i, j = n
+            return self.listseqs[i][j]
+        elif type(n) is int: return self.listseqs[n]
+        return None
+    
+    def __str__(self):
+        res = ""
+        for seq in self.listseqs:
+            res += "\n" + seq 
+        return res
+    
+    def num_seqs(self):
+        return len(self.listseqs)
+
+    def column (self, indice):
+        res = []
+        for k in range(len(self.listseqs)):
+            res.append(self.listseqs[k][indice])
+        return res
+    
+    def consensus (self):
+        cons = ""
+        for i in range(len(self)):
+            cont = {}
+            for k in range(len(self.listseqs)):
+                c = self.listseqs[k][i]
+                if c in cont:
+                    cont[c] = cont[c] + 1
+                else: 
+                    cont[c] = 1
+            maximum = 0
+            cmax = None
+            for ke in cont.keys():
+                if ke != "-" and cont[ke] > maximum: 
+                    maximum = cont[ke]
+                    cmax = ke
+            cons = cons + cmax
+        return cons
+
+
+    ## exercise 4 of chapter 8
+    def richpolarbasic (self):
+        res = []
+        for c in range(len(self)):
+            col = self.column(c)
+            num = 0
+            for aa in col: 
+                if aa == "R" or aa== "H" or aa == "K": 
+                    num += 1
+            if num >= len(col)/2 : res.append(c)
+        return res
+
 class BinaryTree:
 
     def __init__(self, val, dist = 0, left = None, right = None):
